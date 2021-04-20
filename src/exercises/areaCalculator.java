c91cfb32dc1d1415786e42035c4dcc4ef18afa57
Jarod Waggener <jarod2228@rocketmail.com>
2021-04-20 13:19:00 -0500

    finished studio and exercises

---
+++ src/exercises/areaCalculator.java
@@ -0,0 +1,11 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class areaCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the length of the rectangle");
+
+    }
+}
---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,13 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the radius of the circle:");
+        double radius = input.nextDouble();
+        double pi = 3.14;
+        System.out.println("The area of the circle with radius " + radius + " is " + Circle.getArea(radius));
+    }
+}
