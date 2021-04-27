b2452605f7fd4bbabe90b06b6ec0cac848df6e68
Jarod Waggener <jarod2228@rocketmail.com>
2021-04-27 11:18:27 -0500

    did more things to the notes FirstSquare.java and SecondSquare.java which are brainstorm things for the restaurant menu dealio

---
+++ src/org/launchcode/java/demos/lsn1datatypes/FirstSquare.java
@@ -0,0 +1,21 @@
+package org.launchcode.java.demos.lsn1datatypes;
+
+import java.lang.reflect.Array;
+
+public class FirstSquare {
+}
+
+               HashMap<String, Array[5]> MenuItem = new HashMap<>()
+- private double price
+- private String = name
+- private String description
+- private String category(appetizer, main course, dessert)
+- private boolean isNew
+
+constructor (double price, String description, String category, boolean isNew) {
+    this.price = price;
+    this.name = name;
+    this.description = description;
+    this.category = category;
+    this.isNew = isNew;
+        }
---
+++ src/org/launchcode/java/demos/lsn1datatypes/SecondSquare.java
@@ -0,0 +1,20 @@
+package org.launchcode.java.demos.lsn1datatypes;
+
+public class SecondSquare {
+}
+
+               arrayList<MenuItem> Menu
+- consists of several MenuItem items
+- displays when it was last updated
+
+                methods
+addItem(MenuItem item) {
+    menuName.add(item);
+        }
+removeItem(MenuItem item) {
+    menuName.remove(item);
+        }
+printMenu() {
+    use a for each loop to print the menu items from the array list
+    system.out.println(menuName[i]);
+        }
