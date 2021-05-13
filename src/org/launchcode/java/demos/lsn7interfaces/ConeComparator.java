3355a0e2860c076c14cc0bfad23a13236e22954f
Jarod Waggener <jarod2228@rocketmail.com>
2021-05-13 15:56:33 -0500

    nearly done with interface/polymorphism exercise.

---
+++ src/org/launchcode/java/demos/lsn7interfaces/ConeComparator.java
@@ -0,0 +1,10 @@
+package org.launchcode.java.demos.lsn7interfaces;
+
+import java.util.Comparator;
+
+public class ConeComparator implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        // do stuff like flavor comparator did but by cost least to most expensive;
+    }
+}
